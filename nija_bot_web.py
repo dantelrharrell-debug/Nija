00f19587bf4c1e18ffe2db4e3fe839ca571b6778
Dante <dantelrharrell@gmail.com>
2025-10-21 01:01:48 -0700

    Update nija_bot_web.py

--- nija_bot_web.py
+++ nija_bot_web.py
@@ -1,3 +1,10 @@
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "vendor"))
+
+from coinbase_advanced_py.client import CoinbaseClient
+
+print("✅ CoinbaseClient loaded")
+
 import sys, os, time, threading, signal
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
