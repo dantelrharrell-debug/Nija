c25e4ae04e3aa3bc1bd52f4d94aa129649590b9d
Dante <dantelrharrell@gmail.com>
2025-10-20 21:27:28 -0700

    Update __init__.py

--- nija_bundle/nija_bundle/vendor/coinbase_advanced_py/__init__.py
+++ nija_bundle/nija_bundle/vendor/coinbase_advanced_py/__init__.py
@@ -1,2 +1,8 @@
-# __init__.py
-from .client import CoinbaseClient
+class CoinbaseClient:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def get_spot_price(self, currency_pair="BTC-USD"):
+        # example implementation
+        return {"amount": 30000.0}
