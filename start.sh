300d587eafbfff89aa3a8e5345a5d0e1b674ee82
Dante <dantelrharrell@gmail.com>
2025-10-28 15:31:36 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Activate virtualenv
-source ./.venv/bin/activate
-
 echo "🌟 Starting Nija bot..."
-
-# Start your bot (replace nija_live_snapshot.py if different)
 python3 nija_live_snapshot.py
