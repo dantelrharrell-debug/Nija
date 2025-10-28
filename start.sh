b361dbf6534da09dd509aacacf9657c2cf9fa3d7
Dante <dantelrharrell@gmail.com>
2025-10-28 16:06:25 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# start.sh
+
+echo "🌟 Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
 echo "🌟 Starting Nija bot..."
 python3 nija_live_snapshot.py
