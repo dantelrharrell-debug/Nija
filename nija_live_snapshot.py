8e464380ae28087913b54923ed0c6f099718305d
Dante <dantelrharrell@gmail.com>
2025-10-28 11:31:40 -0700

    Update nija_live_snapshot.py

--- nija_live_snapshot.py
+++ nija_live_snapshot.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-import logging
 from nija_client import client, start_trading
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 if __name__ == "__main__":
-    logger.info("🌟 Starting Nija bot...")
     start_trading()
