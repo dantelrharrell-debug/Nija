9b83c07a7dc94521d4d92fe6ec441cd2cabacce4
Dante <dantelrharrell@gmail.com>
2025-11-07 21:43:49 -0700

    Update nija_client.py

--- nija_client.py
+++ nija_client.py
@@ -1,11 +1,38 @@
-# --- BEGIN get_accounts patch ---
-from nija_client import CoinbaseClient as OriginalCoinbaseClient
+import os
+import requests
+import hmac
+import hashlib
+import time
+import json
+import base64
 
-# Only patch once
-if not hasattr(OriginalCoinbaseClient, "get_accounts"):
-    def get_accounts(self, *args, **kwargs):
-        # Redirect to fetch_accounts
-        return self.fetch_accounts(*args, **kwargs)
-    
-    setattr(OriginalCoinbaseClient, "get_accounts", get_accounts)
-# --- END get_accounts patch ---
+class CoinbaseClient:
+    def __init__(self):
+        self.api_key = os.getenv("COINBASE_API_KEY")
+        self.api_secret = os.getenv("COINBASE_API_SECRET")
+        self.passphrase = os.getenv("COINBASE_API_PASSPHRASE")
+        self.base_url = os.getenv("COINBASE_API_BASE", "https://api.coinbase.com")
+
+        if not all([self.api_key, self.api_secret, self.passphrase]):
+            raise ValueError("Coinbase API credentials are not fully set")
+
+    def _get_headers(self, method: str, path: str, body=""):
+        timestamp = str(int(time.time()))
+        message = timestamp + method.upper() + path + body
+        hmac_key = base64.b64decode(self.api_secret)
+        signature = hmac.new(hmac_key, message.encode(), hashlib.sha256).hexdigest()
+        return {
+            "CB-ACCESS-KEY": self.api_key,
+            "CB-ACCESS-SIGN": signature,
+            "CB-ACCESS-TIMESTAMP": timestamp,
+            "CB-ACCESS-PASSPHRASE": self.passphrase,
+            "Content-Type": "application/json",
+        }
+
+    def get_accounts(self):
+        path = "/v2/accounts"
+        headers = self._get_headers("GET", path)
+        url = self.base_url + path
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
