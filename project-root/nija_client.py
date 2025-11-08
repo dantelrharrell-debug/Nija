fd9a37995814becadf1cc2de516b59c002f9135c
Dante <dantelrharrell@gmail.com>
2025-11-07 21:39:26 -0700

    Update nija_client.py

--- project-root/nija_client.py
+++ project-root/nija_client.py
@@ -1,3 +1,15 @@
+# --- BEGIN get_accounts patch ---
+from nija_client import CoinbaseClient as OriginalCoinbaseClient
+
+# Only patch once
+if not hasattr(OriginalCoinbaseClient, "get_accounts"):
+    def get_accounts(self, *args, **kwargs):
+        # Redirect to fetch_accounts
+        return self.fetch_accounts(*args, **kwargs)
+    
+    setattr(OriginalCoinbaseClient, "get_accounts", get_accounts)
+# --- END get_accounts patch ---
+
 # nija_client.py
 import os
 import time
