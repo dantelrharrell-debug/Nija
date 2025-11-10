d239c1662b191a0b16e546e75a022871f148def4
Dante <dantelrharrell@gmail.com>
2025-11-10 13:47:45 -0700

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -1,8 +1,8 @@
 from nija_client import CoinbaseClient
 
 def main():
-    client = CoinbaseClient(advanced=True, debug=True)
-    accounts = client.fetch_advanced_accounts()
+    client = CoinbaseClient(debug=True)
+    accounts = client.fetch_accounts()
     print("Fetched accounts:", accounts)
 
 if __name__ == "__main__":
