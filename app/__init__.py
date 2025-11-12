a3a6d16574a389eb684aa65e663e03ad64c811ec
Dante <dantelrharrell@gmail.com>
2025-11-12 11:27:49 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
 # app/__init__.py
-# marks the `app` folder as a Python package
-__all__ = ["nija_client", "start_bot_main", "webhook"]
+"""
+Package initializer for the 'app' package.
+"""
+__all__ = ["nija_client", "start_bot_main"]
