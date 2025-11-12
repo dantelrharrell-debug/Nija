c6eb1f4bdd480031aa041fdb4f8fc96643184d32
Dante <dantelrharrell@gmail.com>
2025-11-11 18:27:56 -0700

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -1,4 +1,9 @@
-from app.start_bot_main import main
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "app"))
+
+from start_bot_main import main
 
 if __name__ == "__main__":
     main()
