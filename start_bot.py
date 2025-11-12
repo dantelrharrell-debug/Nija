8199c53832bd405bbf3f7846926ee728b6643b2c
Dante <dantelrharrell@gmail.com>
2025-11-11 19:12:21 -0700

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-# Add app folder to Python path
+# Add the app folder to the Python module search path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "app"))
 
-# Now import from the app folder
+# Now we can import start_bot_main
 from start_bot_main import main
 
 if __name__ == "__main__":
