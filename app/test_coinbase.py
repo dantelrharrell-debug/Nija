6dd358372c7f3a748e081765f730fe5d8b988eef
Dante <dantelrharrell@gmail.com>
2025-11-13 17:00:52 -0700

    Update test_coinbase.py

--- app/test_coinbase.py
+++ app/test_coinbase.py
@@ -1,14 +1,10 @@
 from coinbase.rest import RESTClient
 import os
-from loguru import logger
 
 api_key = os.environ.get("COINBASE_API_KEY")
 api_secret = os.environ.get("COINBASE_PEM_CONTENT")
 
 client = RESTClient(api_key=api_key, api_secret=api_secret)
 
-try:
-    accounts = client.get_accounts()
-    logger.info(accounts)
-except Exception as e:
-    logger.error(e)
+accounts = client.get_accounts()
+print(accounts)
