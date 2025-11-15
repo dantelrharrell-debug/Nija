cae679bfa4144e8c6856e3ab7ce42a6d25efd16c
Dante <dantelrharrell@gmail.com>
2025-11-14 21:09:54 -0700

    Update check_pem.py

--- check_pem.py
+++ check_pem.py
@@ -2,10 +2,12 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 
-with open("coinbase.pem","rb") as f: data = f.read()
-print("PEM length bytes:", len(data))
+path = "coinbase.pem"
+with open(path, "rb") as f:
+    data = f.read()
+print("PEM byte length:", len(data))
 try:
     key = serialization.load_pem_private_key(data, password=None, backend=default_backend())
     print("PEM loads OK.")
 except Exception as e:
-    print("PEM load failed:", e)
+    print("PEM load failed:", repr(e))
