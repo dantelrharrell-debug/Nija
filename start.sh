884ba121667a871cf9122d6731bdacd3edc375e6
Dante <dantelrharrell@gmail.com>
2025-11-19 06:25:54 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "[NIJA] Installing Coinbase Advanced SDK..."
+python3 -m pip install --upgrade pip
+python3 -m pip install "git+https://${GITHUB_PAT}@github.com/coinbase/coinbase-advanced-python.git"
+
 echo "[NIJA] Starting Nija Trading Bot..."
-# Using system Python, no venv
 python3 nija_render_worker.py
