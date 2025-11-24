fd4828dd9827d3f750fd0271e3ef41ddd4aed400
Dante <dantelrharrell@gmail.com>
2025-11-24 07:33:16 -0700

    Update Gunicorn configuration parameters

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,10 @@
+# --- Gunicorn configuration ---
 bind = "0.0.0.0:5000"
 workers = 1
-worker_class = "sync"
 threads = 1
+worker_class = "sync"
 timeout = 30
+keepalive = 2
+loglevel = "debug"
 accesslog = "-"
 errorlog = "-"
-loglevel = "debug"
