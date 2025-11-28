ad164bdb4a3be4eec86674f4c40e8ad93f789df8
Dante <dantelrharrell@gmail.com>
2025-11-28 16:51:03 -0700

    Initialize Flask app with a basic route

--- app/nija_client/__init__.py
+++ app/nija_client/__init__.py
@@ -1,1 +1,8 @@
+# app/nija_client/__init__.py
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({"status": "ok", "service": "Nija Bot Running!"})
