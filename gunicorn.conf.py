1829e5a0e8bad45901bc917687aeb300516e9b85
Dante <dantelrharrell@gmail.com>
2025-11-28 17:47:15 -0700

    Change Gunicorn bind port from 5000 to 8080

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,9 @@
-# gunicorn_conf.py
-bind = "0.0.0.0:5000"  # Match EXPOSE 5000
+# gunicorn.conf.py
+bind = "0.0.0.0:8080"
 workers = 2
-worker_class = "gthread"
 threads = 2
-worker_connections = 1000
-timeout = 120
-graceful_timeout = 30
-keepalive = 2
-accesslog = "-"
-errorlog = "-"
+worker_class = "gthread"
 loglevel = "debug"
-capture_output = True
+
+# point to web.wsgi:application by default (our entrypoint also uses this)
+wsgi_app = "web.wsgi:application"
