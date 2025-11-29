5d93d686ecc5e33a7f827c91f66a7c68f363a065
Dante <dantelrharrell@gmail.com>
2025-11-29 11:51:28 -0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,14 @@
-wsgi_app = "app.wsgi:wsgi_app"
+# gunicorn.conf.py
+
+# Bind to port 8080
 bind = "0.0.0.0:8080"
+
+# Worker config
 workers = 2
 worker_class = "gthread"
 threads = 1
 timeout = 120
-loglevel = "debug"
+keepalive = 2
+
+# WSGI app
+wsgi_app = "web.wsgi:app"  # <-- IMPORTANT: points to your app instance
