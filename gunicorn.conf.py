2a597b35b0e7a5fd2e0c53a0b21d8c5ed7791455
Dante <dantelrharrell@gmail.com>
2025-11-29 12:15:19 -0700

    Update WSGI app comment in gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,3 @@
-# gunicorn.conf.py
 bind = "0.0.0.0:8080"
 workers = 2
 worker_class = "gthread"
@@ -6,5 +5,5 @@ threads = 1
 timeout = 120
 keepalive = 2
 
-# MUST point to your app
+# Point to your WSGI app
 wsgi_app = "web.wsgi:app"
