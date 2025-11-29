5b80452d299240e56def58c8279c4379559814d0
Dante <dantelrharrell@gmail.com>
2025-11-28 22:16:01 -0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,10 @@
+# gunicorn.conf.py
 bind = "0.0.0.0:8080"
 workers = 2
 worker_class = "gthread"
 threads = 1
 timeout = 30
-capture_output = True
-loglevel = "debug"
+reload = False
+
+# Must match your wsgi module path
+wsgi_app = "web.wsgi:application"
