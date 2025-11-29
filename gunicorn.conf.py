8167f302153b6f6874ef33af4ac656dadc4f0c53
Dante <dantelrharrell@gmail.com>
2025-11-29 02:52:11 -0700

    Refactor gunicorn config to use environment variables
    Updated Gunicorn configuration to use environment variables for bind, workers, threads, timeout, and loglevel settings.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,15 +1,17 @@
 # gunicorn.conf.py
+import os
 
-bind = "0.0.0.0:8080"
-workers = 2
-worker_class = "gthread"
-threads = 1
-timeout = 30
-graceful_timeout = 30
-keepalive = 2
-accesslog = "-"
-errorlog = "-"
-loglevel = "debug"
+PORT = os.environ.get("PORT") or "8080"
 
-# Point to your WSGI app
-wsgi_app = "web.wsgi:application"
+bind = f"0.0.0.0:{PORT}"
+workers = int(os.environ.get("GUNICORN_WORKERS", "2"))
+threads = int(os.environ.get("GUNICORN_THREADS", "2"))
+worker_class = os.environ.get("GUNICORN_WORKER_CLASS", "gthread")
+timeout = int(os.environ.get("GUNICORN_TIMEOUT", "30"))
+loglevel = os.environ.get("GUNICORN_LOGLEVEL", "debug")
+capture_output = True
+accesslog = "-"   # logs to stdout
+errorlog = "-"    # logs to stdout
+
+# make sure default proc points to our wsgi module/object
+default_proc_name = "web.wsgi:application"
