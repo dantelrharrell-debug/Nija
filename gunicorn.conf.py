b6600a1c465f40ca81b5b0b2445b2f7d9b0f72aa
Dante <dantelrharrell@gmail.com>
2025-11-29 03:51:52 -0700

    Simplify gunicorn configuration settings

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,8 @@
-# gunicorn.conf.py - conservative production settings
+# gunicorn.conf.py
 bind = "0.0.0.0:8080"
 workers = 2
 worker_class = "gthread"
 threads = 1
 timeout = 30
-keepalive = 2
 loglevel = "debug"
 capture_output = True
-errorlog = "-"
-accesslog = "-"
-chdir = "/app"
