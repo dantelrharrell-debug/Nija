a1a5d23c21916a9296cf53860a635132541620ab
Dante <dantelrharrell@gmail.com>
2025-11-30 17:47:22 -0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,9 @@
-# gunicorn.conf.py
-wsgi_app = "web.wsgi:app"
 bind = "0.0.0.0:5000"
 workers = 2
 worker_class = "gthread"
 threads = 2
-worker_connections = 1000
 timeout = 120
-graceful_timeout = 30
-capture_output = True
 loglevel = "debug"
 accesslog = "-"
 errorlog = "-"
+capture_output = True
