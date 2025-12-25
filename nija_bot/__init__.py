d682885064b40184e64bcb9775cf9b47c37354b5
Dante <dantelrharrell@gmail.com>
2025-12-25 03:54:37 -0700

    Create nija_bot package initialization file

--- nija_bot/__init__.py
+++ nija_bot/__init__.py
@@ -1,12 +1,9 @@
 """
 NIJA Bot Package
-Main package initialization for NIJA trading bot modules
+Trading bot module initialization
 """
 
 __version__ = "1.0.0"
-__author__ = "NIJA Trading System"
+__all__ = ["NijaStrategy"]
 
-# Package-level imports for convenience
 from .strategy import NijaStrategy
-
-__all__ = ['NijaStrategy']
