76446a1aa5f86fb67f58557e73df2998e3e8642c
Dante Harrell <dantelrharrell@gmail.com>
2026-08-30 12:39:58 -0700

    Chain Kraken monitoring fairness v297

--- bot/production_runtime_convergence_v88_patch.py
+++ bot/production_runtime_convergence_v88_patch.py
@@ -197,6 +197,9 @@ def _install_kraken_user_supervision() -> bool:
         if installed:
             from bot import runtime_dust_position_policy_convergence_v296_patch as v296
             installed = bool(v296.install_import_hook())
+        if installed:
+            from bot import runtime_kraken_monitoring_fairness_v297_patch as v297
+            installed = bool(v297.install_import_hook())
     except Exception as exc:
         LOGGER.warning(
             "KRAKEN_USER_SUPERVISION_V88_INSTALL_FAILED marker=%s error=%s:%s",
@@ -209,7 +212,7 @@ def _install_kraken_user_supervision() -> bool:
         with _LOCK:
             _KRAKEN_SUPERVISION_INSTALLED = True
         LOGGER.critical(
-            "KRAKEN_USER_SUPERVISION_V88_CHAINED marker=%s source=v86+v90+v266+v282+v285+v286+v287+v288+v289+v290+v292+v293+v294+v295+v296 "
+            "KRAKEN_USER_SUPERVISION_V88_CHAINED marker=%s source=v86+v90+v266+v282+v285+v286+v287+v288+v289+v290+v292+v293+v294+v295+v296+v297 "
             "authenticated_reconnect_only=true canonical_rebuild=true writer_scoped=true "
             "state_sensitive_diagnostics=true connected_poll_private_io_bounded=true "
             "authoritative_user_position_proof_required=true current_snapshot_required=true "
@@ -220,7 +223,8 @@ def _install_kraken_user_supervision() -> bool:
             "authoritative_stale_tracker_cleanup=true local_read_contention_retry=true "
             "kraken_transport_timeout_bound=true credential_scoped_private_serialization=true "
             "platform_user_position_sync_isolation=true okx_fill_history_cost_basis=true "
-            "dust_position_policy_converged=true lock_bypass=false lock_force_release=false "
+            "dust_position_policy_converged=true authoritative_monitoring_priority=true "
+            "kraken_balance_single_flight=true lock_bypass=false lock_force_release=false "
             "platform_activation_preserved_for_user_local_failures=true",
             MARKER,
         )
@@ -300,7 +304,8 @@ def install_import_hook() -> bool:
         "kraken_read_contention_recovery_v290=true kraken_transport_timeout_v292=true "
         "kraken_credential_lock_scope_v293=true position_sync_isolation_v294=true "
         "okx_cost_basis_recovery_v295=true dust_position_policy_convergence_v296=true "
-        "stale_log_filter=true risk_gates_unchanged=true nonce_gates_unchanged=true",
+        "kraken_monitoring_fairness_v297=true stale_log_filter=true "
+        "risk_gates_unchanged=true nonce_gates_unchanged=true",
         MARKER,
     )
     return True
