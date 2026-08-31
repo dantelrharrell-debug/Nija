f8927f15f546436bfa1fe971369a11d52777f389
Dante Harrell <dantelrharrell@gmail.com>
2026-08-31 10:37:28 -0700

    Chain Kraken Balance epoch handoff into production convergence

--- bot/production_runtime_convergence_v88_patch.py
+++ bot/production_runtime_convergence_v88_patch.py
@@ -207,6 +207,9 @@ def _install_kraken_user_supervision() -> bool:
             from bot import runtime_kraken_credential_read_convergence_v299_patch as v299
             installed = bool(v299.install_import_hook())
         if installed:
+            from bot import runtime_kraken_balance_epoch_handoff_v312_patch as v312
+            installed = bool(v312.install_import_hook())
+        if installed:
             from bot import runtime_registered_platform_position_completeness_v302_patch as v302
             installed = bool(v302.install_import_hook())
         if installed:
@@ -224,7 +227,7 @@ def _install_kraken_user_supervision() -> bool:
         with _LOCK:
             _KRAKEN_SUPERVISION_INSTALLED = True
         LOGGER.critical(
-            "KRAKEN_USER_SUPERVISION_V88_CHAINED marker=%s source=v86+v90+v266+v282+v285+v286+v287+v288+v289+v290+v292+v293+v294+v295+v296+v297+v298+v299+v302+v303 "
+            "KRAKEN_USER_SUPERVISION_V88_CHAINED marker=%s source=v86+v90+v266+v282+v285+v286+v287+v288+v289+v290+v292+v293+v294+v295+v296+v297+v298+v299+v312+v302+v303 "
             "authenticated_reconnect_only=true canonical_rebuild=true writer_scoped=true "
             "state_sensitive_diagnostics=true connected_poll_private_io_bounded=true "
             "authoritative_user_position_proof_required=true current_snapshot_required=true "
@@ -238,6 +241,7 @@ def _install_kraken_user_supervision() -> bool:
             "dust_position_policy_converged=true authoritative_monitoring_priority=true "
             "kraken_balance_single_flight=true inflight_current_snapshot_truth=true "
             "credential_scoped_monitoring_coordination=true same_credential_balance_coalescing=true "
+            "authenticated_balance_epoch_handoff=true existing_flight_timeout_recovery=true "
             "registered_platform_position_denominator=true disconnected_registered_explicit_false=true "
             "heartbeat_position_cap_result_bridge=true "
             "snapshot_ttl_unchanged=true lock_bypass=false lock_force_release=false "
@@ -321,8 +325,8 @@ def install_import_hook() -> bool:
         "kraken_credential_lock_scope_v293=true position_sync_isolation_v294=true "
         "okx_cost_basis_recovery_v295=true dust_position_policy_convergence_v296=true "
         "kraken_monitoring_fairness_v297=true kraken_inflight_snapshot_truth_v298=true "
-        "kraken_credential_read_convergence_v299=true registered_platform_position_completeness_v302=true "
-        "heartbeat_position_cap_result_bridge_v303=true "
+        "kraken_credential_read_convergence_v299=true kraken_balance_epoch_handoff_v312=true "
+        "registered_platform_position_completeness_v302=true heartbeat_position_cap_result_bridge_v303=true "
         "stale_log_filter=true risk_gates_unchanged=true nonce_gates_unchanged=true",
         MARKER,
     )
