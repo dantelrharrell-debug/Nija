7abaaf90d7e36bcc65fb2b0f3851de776b33c417
Dante Harrell <dantelrharrell@gmail.com>
2026-09-22 20:30:16 -0700

    Wire adaptive strategy ranking into detector pipeline

--- bot/control/signal_pipeline.py
+++ bot/control/signal_pipeline.py
@@ -88,6 +88,7 @@ from bot.control.signal_scoring import SignalScoringEngine
 from bot.control.situation_analysis import SituationAnalysisEngine
 from bot.control.strategy_registry import StrategyDetectorRegistry
 from bot.control.strategy_signal import StrategySignal
+from bot.adaptive_strategy_allocator import get_adaptive_strategy_allocator
 
 logger = logging.getLogger("nija.control.pipeline")
 
@@ -143,6 +144,7 @@ class SignalPipeline:
         self._scoring_engine = SignalScoringEngine()
         self._confirmation_engine = ConfirmationEngine()
         self._situation_engine = SituationAnalysisEngine()
+        self._adaptive_strategy_allocator = get_adaptive_strategy_allocator()
         self._redis         = redis_client
         self._context_authorizer = context_authorizer
         self._lock          = threading.Lock()
@@ -272,7 +274,9 @@ class SignalPipeline:
 
         if not ranked:
             return None
-        best_signal, best_score = max(ranked, key=lambda row: row[1])
+
+        adaptive_ranked = self._adaptive_strategy_allocator.rank(ranked)
+        best_signal, best_score, adaptive_detail = adaptive_ranked[0]
         strategy_signal_id = best_signal.strategy_signal_id
         if best_signal.trading_context.scope_key != trading_context.scope_key:
             logger.warning("PIPELINE_REJECT stage=context reason=detector_context_mismatch")
@@ -348,6 +352,7 @@ class SignalPipeline:
                 **execution_meta,
                 "entry_price": entry_price,
                 "signal_score": best_score,
+                "adaptive_strategy": adaptive_detail.to_dict(),
             },
             execution_mode=trading_context.mode,
         )
